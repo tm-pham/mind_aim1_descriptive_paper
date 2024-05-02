d789a7845d2ff0df67ede17f0c6e4098f496e3d7
tm-pham <thi.mui.pham@posteo.de>
2024-05-02 11:37:57 -0400

    Added file that saves all relevant global variables such as colors and order of organisms

---
+++ code/mind_global_variables.R
@@ -0,0 +1,19 @@
+# Thi Mui Pham, t.m.pham@hsph.harvard.edu
+# ============================================================================ #
+# Global variables for all scripts
+# ============================================================================ #
+
+# Global variables 
+susc_colors = c("#35978F", "#800000", "#888888")
+fill_color ="#eda361"
+
+# Organisms of interest
+bugs_ordered <- c("Staphylococcus aureus", 
+                  "Enterococcus faecalis", 
+                  "Enterococcus faecium", 
+                  "Escherichia coli", 
+                  "Klebsiella pneumoniae", 
+                  "Enterobacter cloacae", 
+                  "Serratia marcescens", 
+                  "Pseudomonas aeruginosa", 
+                  "Acinetobacter Sp.")
