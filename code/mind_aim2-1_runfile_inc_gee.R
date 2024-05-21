fe298b451895e337c18ca5b43e591f82f35bc5bb
tm-pham <thi.mui.pham@posteo.de>
2024-05-21 15:23:58 -0400

    Updated GEE results output by adding p-values to the output.

--- code/mind_aim2-1_runfile_inc_gee.R
+++ code/mind_aim2-1_runfile_inc_gee.R
@@ -4,23 +4,39 @@
 # ============================================================================ #
 # This file uses mind_aim2-1_function_inc_gee.R to run the GEE analysis for 
 # organisms of interest. 
-# 1. Prepare input and output data (lines 26-43). 
-# 2. Run GEE analysis for each organism (lines 44-96). 
-# 3. Compute multiplicative Average Annual Percentage Change (lines 98-141). 
-# 4. Save output data (lines 142-150). 
+# Note that the code cannot be run as such since the underlying data is not 
+# publicly available. 
+# The code 
+# 1. Prepare input and output data (lines 41ff.). 
+# 2. Run GEE analysis for each organism (lines 59ff.). 
+# 3. Compute multiplicative Average Annual Percentage Change (lines 113ff.). 
+# 4. Save output data (lines 157ff.). 
 #    (Output is an Excel sheet with mean values and 95% confidence intervals) 
 # ============================================================================ #
-setwd("P:/ORD_Samore_202109019D/Mui/")
+setwd("/Users/tm-pham/academia/hsph/mind/publications/aim2-1/")
 source("code/packages.R")
 source("code/plotting_template.R")
 source("code/mind_aim2-1_function_df_inc.R")
 source("code/mind_aim2-1_function_inc_gee.R")
+source("code/mind_aim2-1_function_aapc.R")
 
 # Load data
 load("data/bugs_drugs.RData")                  # List of organisms and corresponding antibiotic (classes)
+# Note that the following three data sets are not publicly available 
 load("data/micro_data_inc30d.RData")           # micro_data_inc30, microbiology data
 load("data/mind_n_hosp.RData")                 # Number of hospital admissions data 
 load("data/mind_fac_characteristics_yr.RData") # Facility characteristics per year
+
+# Organisms of interest
+bugs_ordered <- c("Staphylococcus aureus", 
+                  "Enterococcus faecalis", 
+                  "Enterococcus faecium", 
+                  "Escherichia coli", 
+                  "Klebsiella pneumoniae", 
+                  "Enterobacter cloacae", 
+                  "Serratia marcescens", 
+                  "Pseudomonas aeruginosa", 
+                  "Acinetobacter Sp.")
 
 # ---------------------------------------------------------------------------- #
 # Prepare input data 
@@ -96,7 +112,7 @@ for(i in 1:length(bugs)){
 # ---------------------------------------------------------------------------- #
 # 3. Average annual change (%)
 df_gee_results <- NULL
-for(bug in bugs_sorted){
+for(bug in bugs_ordered){
   (overall_aapc <- aapc.function(inc_gee_models_org[[bug]], specs = "~time_1", var="time_1",
                                  quarter = F))
   df_gee_results <- rbind(df_gee_results, cbind(org = bug, 
@@ -124,25 +140,38 @@ for(bug in bugs_sorted){
                                                 p_onset_aapc))
 }
 
+df_gee_results$p_value <- round(df_gee_results$p_value, 4)
+
 # Create AAPC output table
 output_table <- cbind(df_gee_results, text = apply(df_gee_results, 1, 
-                                                   function(x) paste0(x[4], "% (95%CI: [", x[5], ", ", x[6], "])"))) %>% 
-  mutate(org = factor(org, levels=bugs_sorted)) %>% 
+                                                   function(x) paste0(x[4], "% (", x[5], ", ", x[6], ")"))) %>% 
+  mutate(org = factor(org, levels=bugs_ordered), 
+         p_value = round(p_value, 4)) %>% 
   arrange(org)
 
 for(i in 1:nrow(output_table)){
   if(output_table[i, "asymp.LCL"]<=0 & 0<=output_table[i, "asymp.UCL"]){
     output_table[i, "text.star"] <- output_table[i, "time.trend"]
   }else{
-    output_table[i, "text.star"] <- paste0(output_table[i, "time.trend"], "*")
+    if(output_table[i, "p_value"]>0.000){
+      output_table[i, "text"] <- paste0(output_table[i, "text"], "* p-value = ", output_table[i, "p_value"])
+    }else{
+      output_table[i, "text"] <- paste0(output_table[i, "text"], "*")
+    }
+
   }
 }
+
+View(output_table %>% 
+  select(org, variable, onset, text) %>% 
+  pivot_wider(id_cols = c(org, variable), values_from = text, names_from = onset) %>% 
+  mutate(variable = factor(variable, levels = c("pre-pandemic", "pandemic"))))
+
 
 # ---------------------------------------------------------------------------- #
 # 4. Save Data
 # Save as Excel sheet
-write.xlsx(output_table, sep="\t", file = "data/mind_inc_gee_results_by_org_onset_geeasy_year_Dec23.xlsx", 
-           quote=F, row.names=F, col.names=T)
+write.xlsx(output_table, file = "results/mind_inc_gee_results_by_org_onset_geeasy_year_May2024.xlsx", row.names=F, col.names=T)
 
 # Save as RData 
 save(inc_gee_models_org, file="data/mind_inc_gee_models_orgs_ar1_geeasy_year.RData")
